f67f76d3cc12d7fa1a842984486003ea4390f69d
ethan <ethancorn82@gmail.com>
2026-02-20 19:05:01 -0600

    completed state check, need to add key generation to finish init.py

--- init.py
+++ init.py
@@ -2,12 +2,39 @@
 # see if there are known keys or not
 # if not, generate keys and create new .pem file in /keys
 from pathlib import Path
+import shutil
 
-keys_path = "DEMP/keys"
-pub_key_file = "DEMP/keys/pub_key.pem"
-priv_key_file = "DEMP/keys/priv_key.pem"
-if keys_path.exists():
-    print("keys found...")
+application_folder = Path(__file__).resolve().parent
+keys_folder_path = application_folder/"keys"
+pub_key_file = application_folder/"keys/pub_key.pem"
+priv_key_file = application_folder/"keys/priv_key.pem"
+
+def generate_keys_folder():
+    print(f"no key folder found : \n\t- creating keys folder @ {keys_folder_path}")
+    print(f"\t- creating private key file @ {priv_key_file}")
+    print(f"\t- creating public key file @ {pub_key_file}")
+    keys_folder_path.mkdir(exist_ok=True)
+    priv_key_file.touch()
+    pub_key_file.touch()
+    if pub_key_file.exists() and priv_key_file.exists():
+        print("fresh key files successfully created")
+    else:
+        print(f"Please delete {keys_folder_path} and try again.")
+def check_keypair_corruption():
+    if pub_key_file.exists() and priv_key_file.exists():
+        print("public key : [ok] / private key : [ok]")
+    if not pub_key_file.exists():
+        print("public key : [bad] / private key : [ok]")
+        generate_keys_folder()
+    else:
+        print("public key : [ok] / private key : [bad]")
+    print("found key pair corruption :")
+    print("\t- deleting old keys & generating new ones")
+    shutil.rmtree(keys_folder_path)
+    generate_keys_folder()
+
+if keys_folder_path.exists():
+    print("keys folder : [ok]")
+    check_keypair_corruption()
 else:
-    open(keys_path, "x") as file:
-    open(keys_path, "x") as file:
+    generate_keys_folder()
