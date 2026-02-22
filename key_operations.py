8eb04a0a54c8e1c349b1d75eabf9c60f35e90734
ethan <ethancorn82@gmail.com>
2026-02-22 13:28:27 -0600

    added key generation

--- key_operations.py
+++ key_operations.py
@@ -3,61 +3,108 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from pathlib import Path
 import shutil
+import os
+from dotenv import load_dotenv
 
-application_folder = Path(__file__).resolve().parent
-keys_folder_path = application_folder/"keys"
-pub_key_file = application_folder/"keys/pub_key.pem"
-priv_key_file = application_folder/"keys/priv_key.pem"
+load_dotenv()
 
-def generate_keys_folder(): #add error correction w/ if statments for creating files
-    if keys_folder_path.exists():
-        shutil.rmtree(keys_folder_path)
+class keys:
+    def generate_keys():
+        private_key = rsa.generate_private_key(
+        public_exponent=65537, #default value
+        key_size=4096
+        )
+        pem_private = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format = serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.BestAvailableEncryption(os.getenv("KEY_FILE_PASSWORD").encode())
+        )
+        public_key = private_key.public_key()
+        pem_public = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        pem_private.splitlines()[0]
+        pem_public.splitlines()[0]
+        return pem_private , pem_public
 
-    print(f"keys folder : [not found]\n\t- creating keys folder @ {keys_folder_path}")
-    print(f"\t- creating private key file @ {priv_key_file}")
-    print(f"\t- creating public key file @ {pub_key_file}")
+class key_file_handler:
+    pub_key_path = application_folder/"keys/pub_key.pem"
+    priv_key_path = application_folder/"keys/priv_key.pem"
+    def load_keys(private_key, public_key): #params are bytes
+        with open (priv_key_path, 'w') as file:
+            file.write(private_key)
+        with open (pub_key_path, 'w') as file:
+            file.write(public_key)
+        
+        if os.path.getsize(priv_key_path) > 0:
+            print("private key loading: [success]")
+        elif os.path.getsize(pub_key_path) > 0:
+            print("public key loading : [success]")
+        else: 
+            print("key loading : [error]")
 
-    keys_folder_path.mkdir(exist_ok=True)
-    priv_key_file.touch()
-    pub_key_file.touch()
+    def check_keypair_corruption():
+        if pub_key_path.exists() and priv_key_path.exists():
+            print("public key : [ok] / private key : [ok]")
+        elif not pub_key_path.exists():
+            print("public key : [bad] / private key : [ok]")
+            self.corruption_message()
+            self.generate_keys_folder()
+        else:
+            print("public key : [ok] / private key : [bad]")
+            corruption_message()
+            generate_keys_folder()
 
-    if pub_key_file.exists() and priv_key_file.exists():
-        print("key files : [creation success]")
-    else:
-        print(f"error: delete {keys_folder_path} and try again.")
+    def corruption_message():
+            print("found key pair corruption :")
+            print("\t- deleting old keys & generating new ones")
 
-def corruption_message():
-        print("found key pair corruption :")
-        print("\t- deleting old keys & generating new ones")
+    print(f"\t- creating private key file @ {priv_key_path}")
+    print(f"\t- creating public key file @ {pub_key_path}")
 
-def check_keypair_corruption():
-    if pub_key_file.exists() and priv_key_file.exists():
-        print("public key : [ok] / private key : [ok]")
-    elif not pub_key_file.exists():
-        print("public key : [bad] / private key : [ok]")
-        corruption_message()
-        generate_keys_folder()
-    else:
-        print("public key : [ok] / private key : [bad]")
-        corruption_message()
-        generate_keys_folder()
 
-def keys_state_check():
-    if keys_folder_path.exists():
-        print("keys folder : [ok]")
-        check_keypair_corruption()
-    else:
-        generate_keys_folder()
+class key_folder_handler:
+    application_folder = Path(__file__).resolve().parent
+    folder_path = application_folder/"keys"
+
+    def generate_folder():
+        if folder_path.exists():
+            shutil.rmtree(folder_path)
+
+        print(f"keys folder : [not found]\n\t- creating keys folder @ {folder_path}")
+
+        folder_path.mkdir(exist_ok=True)
+        priv_key_path.touch()
+        pub_key_path.touch()
+
+        if pub_key_path.exists() and priv_key_path.exists():
+            print("key files : [creation success]")
+        else:
+            print(f"error: delete {folder_path} and try again.")
+        generate_keys()
+
+    def keys_state_check():
+        if folder_path.exists():
+            print("keys folder : [ok]")
+            keys.check_keypair_corruption()
+        else:
+            key_folder_handler.generate_folder()
 
 #hybrid encryption
 #RSA key generation (asymmetric encryption (handshake))
-def generate_keys():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537, #default value
-        key_size=4096
+
+# if keys in key files, load keys in files as keys?
+#def get_keys():
+# else: generate keys and load into files
+
+"""
+this is for loading already generated keys as private key
+with open(priv_key_file, "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
     )
-    with open(private_key_file, "rb") as private_key_file:
-        private_key = serialization.load_pem_private_key(
-            
-        )
-    public_key = private_key.public_key()
+"""
+
+
